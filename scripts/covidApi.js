ecc6d226a61ceb519ea3f829886ef4f356797871
lemonade <adriamartin18@gmail.com>
2020-10-13 14:00:53 +0200

    plasma los resultados de la api

--- scripts/covidApi.js
+++ scripts/covidApi.js
@@ -1,7 +1,11 @@
-const covidApi = "https://api.covid19tracking.narrativa.com/api/2020-10-10/country/spain/region/madrid"
+const covidApi = "https://api.covid19tracking.narrativa.com/api/2020-10-12/country/spain/region/:region/sub-region/:sub-region"
 
 
-const getData = async () => {
+const getData = async (texto) => {
+  var comunidad = document.querySelector('input').value
+  console.log(comunidad)
+  
+  var covidApi = `https://api.covid19tracking.narrativa.com/api/2020-10-12/country/spain/region/${comunidad}`
   
   const responseCovid = await fetch(covidApi)
   const postData = await responseCovid.json()
@@ -12,21 +16,23 @@ const getData = async () => {
   data.map(data => console.log(data.countries.Spain.regions.map(data =>{
   let dataConfirmed = data.today_confirmed;
   let dataDeaths = data.today_deaths;
-  return `${dataConfirmed} ${dataDeaths} listo`
+  //return log.textContent = `${dataConfirmed} ${dataDeaths} listo`
+   if (dataConfirmed > 30000) {return log.textContent = `Casos confirmados:${dataConfirmed} Fallecidos:${dataConfirmed}  Viajar ahí es un canteo. Hazte caso y busca garitos por otro lado.`}
+  else {return log.textContent = `Casos confirmados:${dataConfirmed} Fallecidos:${dataDeaths}   Te renta mazo. Estaría fetén buscarse una keli para unos días.`} 
 })))}
   
-  
-  
- 
- getData(covidApi)
- 
+
 
 
  const input = document.querySelector('input')
  const log = document.getElementById('info-provincia')
+ let regions = document.querySelector('#regions');
 
- input.addEventListener('input', updateValue)
+ 
+ //Primera iteración Hacer un map con el array de objetos de la llamada
+ //Segunda iteración Crear un option
+ //Tercera iteración Meter la infor del map en el innerHTML del option creado
+ //Cuarta iteración Hacerle un appendChild() del option al id de regions
 
- function updateValue(e) {
-    log.textContent = e.srcElement.value
- }
+ input.addEventListener('input', getData)
+
