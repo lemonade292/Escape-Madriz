7a2b51e9c3be4294b92d387d730fbdce6b0ee97a
lemonade <adriamartin18@gmail.com>
2020-10-14 15:25:11 +0200

    select de provincias añadido

--- scripts/covidApi.js
+++ scripts/covidApi.js
@@ -7,7 +7,7 @@ const getData = async (event) => {
   var regions = document.querySelector('.regions').value;
   
   var log = document.querySelector('#info-comunidad');
- // let buscarSubRegions = `https://api.covid19tracking.narrativa.com/api/countries/spain/regions/${regions}/sub_regions`
+  let buscarSubRegions = `https://api.covid19tracking.narrativa.com/api/countries/spain/regions/${regions}/sub_regions`
   var covidApi = `https://api.covid19tracking.narrativa.com/api/2020-10-10/country/spain/region/${regions}`
   //var covidApiSubRegions = `https://api.covid19tracking.narrativa.com/api/2020-10-12/country/spain/region/${regions}/sub_region/${sub_region}`
   
@@ -25,18 +25,43 @@ const getData = async (event) => {
   
   let dataDeaths = data.today_deaths;
     
-   if (dataConfirmed > 30000) {return log.textContent = `Casos confirmados:${dataConfirmed} Fallecidos:${dataDeaths}  Viajar ahí es un canteo. Hazte caso y busca garitos por otro lado.`}
+  if (regions === 'null') {return log.textContent = 'A ver maquina, selecciona un lugar.'}
+  else { if (dataConfirmed > 30000) {return log.textContent = `Casos confirmados:${dataConfirmed} Fallecidos:${dataDeaths}  Viajar ahí es un canteo. Hazte caso y busca garitos por otro lado.`}
   else {return log.textContent = `Casos confirmados:${dataConfirmed} Fallecidos:${dataDeaths}   Te renta mazo. Estaría fetén buscarse una keli para unos días.`} 
-  }
+  } }
 /* } else if(r) {
   console.log(data)
   } */
 //}
 )))}
   
+const getSubregions = async (event) => {
+  
+  var regions = document.querySelector('.regions').value;
+  let buscarSubRegions = `https://api.covid19tracking.narrativa.com/api/countries/spain/regions/${regions}/sub_regions`
+ console.log(regions)
+  fetch(buscarSubRegions)
+  .then(data => data.json())
+  .then(countries => countries.countries[0].spain[regions].map(data =>{
+    
+    let idSubregions = data.id
+    let nameSubregions = data.name
+    let optionId = document.querySelector(".sub-regions");
+    let option = document.createElement("option");
+    option.value = idSubregions
+    option.innerHTML = nameSubregions
+    optionId.appendChild(option) 
+    
+    //return console.log(idSubregions) 
+  
+}))
+
+}
 
 var subButton = document.querySelector('.sub-button')
 subButton.addEventListener('click', getData)
 
+var selectRegion = document.querySelector('.regions')
+selectRegion.addEventListener('change', getSubregions)
 // Option 
 // llamada
